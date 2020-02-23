5e00642aeefbcbcc4954195bb148c568d1b90538
Sinem ertem <wolfsinem@mbp-van-sinem.home>
2020-02-23 15:55:49 +0100

    bijwerken

--- threaded-merge-sort/inleveropgave.py
+++ threaded-merge-sort/inleveropgave.py
@@ -1,12 +1,14 @@
-import threading, logging, time
-import concurrent.futures
+from timeit import default_timer as timer
+import threading, logging
 from random import randint
 from typing import List
-# import sys
-# sys.path.append('/Users/wolfsinem/high-performanced-programming/functions')
-# from sort_functions import merge_sort
+import multiprocessing
+import numpy as np 
+import math
+
+
 """
-Mere sort is een efficient sorteeralgoritme. Dit algoritme neemt een array, 
+Merge sort is een efficient sorteeralgoritme. Dit algoritme neemt een array, 
 spltst deze in (bijna) twee gelijke sub-lijsten om het vervolgens te gaan
 soreren en samen te voegen tot een nieuwe gesorteerde array. De complexiteit
 vandit sorteeralgortime zit in het samenvoegen. 
@@ -23,53 +25,98 @@ Die komt als eerst in de nieuwe array. 14 > 15 -> 20 -> 30 -> 34 -> 51 -> 52 -> 
 def generate_array(size=8,max=20):
     """Create an array of lenght 'size' containing random numbers from the range 0 up to 'max' """
     return [randint(0,max) for _ in range(size)]
-        
-    
-def merge_sort(lijst: List[int]): 
+
+
+def merge_sort(lijst: List[int]):
     """
-    Implementation of the Merge Sort Algortihm: https://www.geeksforgeeks.org/merge-sort/
+    Implementation of the Merge Sort Algortihm.
     Check threaded_merge_sort.png in folder "visuals" for further visual explanation.
-    This algorithm will be used for a new function with threads.
+    This algorithm will be used for a new function with processes.
     """
+    if len(lijst) <=1: 
+        return lijst
 
-    if len(lijst) > 1: 
-        num_lijst = len(lijst)
+    left = merge_sort(lijst[:len(lijst) // 2 ])
+    right = merge_sort(lijst[len(lijst) // 2:])
+
+    return merge(left,right)
+
+
+def merge(left: int, right: int):
+    """Merge the sublists"""
+    lijst = []
+    
+    l=0
+    r=0
+    
+    while l < len(left) and r < len(right):
+        if left[l] <= right[r]:
+            lijst.append(left[l])
+            l += 1
+        else:
+            lijst.append(right[r])
+            r += 1
+    if l == len(left):
+        lijst.extend(right[r:])
+    else:
+        lijst.extend(left[l:])
+    
+    return lijst
+
+
+def split_for_threading(lijst: List[int], processes: int):
+    """
+    Param_ lijst: generated list with x integers
+    Param processes: number of processes being used
+    Var split: divides the list by the amount of processes 
+    """
+    split = int(math.ceil(float(len(lijst)) / processes))
+    splitted_list = [lijst[i * split:(i + 1) * split] for i in range(processes)]
+    
+    return splitted_list
+
+
+def parallel_merge_sort(lijst: List[int], processes: int):
+    """
+    The pool object offers parallelization of the merge sort
+    function. 
+    """
+    pool = multiprocessing.Pool(processes=processes)
+    splitted_list = split_for_threading(lijst,processes)
+    
+    lijst = pool.map(merge_sort, splitted_list)
+
+    while len(lijst) > 1:
+
+        if len(lijst) % 2 == 1:
+            extra = lijst.pop() 
+        else: None
+
+        for i in range(0, len(lijst), 2):
+            lijst = (lijst[i], lijst[i + 1])
         
-        left = lijst[:num_lijst//2]
-        right = lijst[num_lijst//2:]
+        lijst = pool.map(merge,lijst) + ([extra] if extra else [])
 
-        merge_sort(left)
-        merge_sort(right)
-
-        i = 0
-        j = 0
-        k = 0
-        
-        while i < len(left) and j < len(right): 
-            if left[i] < right[j]: 
-                lijst[k] = left[i] 
-                i+=1
-            else: 
-                lijst[k] = right[j] 
-                j+=1
-            k+=1
-        
-        while i < len(left): 
-            lijst[k] = left[i] 
-            i+=1
-            k+=1
-        
-        while j < len(right): 
-            lijst[k] = right[j] 
-            j+=1
-            k+=1
-
-def threaded_merge_sort(lijst: List[int],threads: int):
-    pass
+    return lijst[0]
 
 
-if __name__ == '__main__': 
-    lijst = generate_array() 
-    print ("Given array is: {}".format(lijst))  
-    merge_sort(lijst) 
-    print ("Sorted array is: {}".format(lijst))
+def set_time(function,list_):
+    """function that can be used for each algorithm to measure the execution time"""
+    for i in list_:
+        start_time = timeit.default_timer()
+        function(i)
+        print('This algorithm took {:.4f} seconds'.format((timeit.default_timer() - start_time)))
+
+if __name__ == "__main__":
+    print("Multiprocessed Merge Sort Algorithm")
+    process_amount = multiprocessing.cpu_count()
+    print("Number of processors available on your system: {}".format(str(process_amount)))
+    
+    num_processes = [1,2,4] # my system has a maximum of 4 processes 
+
+    list_to_be_sorted = generate_array() # size = 8, max = 20
+    start_time = timer()
+    sort_process = parallel_merge_sort(list_to_be_sorted,2) # lijst, processes
+    end_time = timer()
+    duration = end_time - start_time
+    print("The parallel merge sort function took {} second(s)".format(duration))
